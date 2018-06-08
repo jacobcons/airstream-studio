1637ac4555d1d5c91e5ec67b49b8d6d4398070bf
Jacob Cons <jacobcons@gmail.com>
2018-06-08 21:15:29 +0100

    contact

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'js', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['jekyll-build', 'sass', 'js'], function() {
     browserSync({
         server: {
             baseDir: '_site'
