223e1a82dbc576996cb2034fc743193a3904eeda
theconartistpi <jacobcons@gmail.com>
2019-02-07 00:34:52 +0000

    Update 2019-02-07T00:34:44.117Z

--- _js/routes/contact.js
+++
@@ -1,9 +0,0 @@
-import Form from '../components/Form'
-import CopyLink from '../components/CopyLink'
-
-export default {
-  init() {
-    const copyLink = new CopyLink()
-		const form = new Form()
-  }
-};
