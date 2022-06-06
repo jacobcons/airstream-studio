2fad2713cd7a7f84d25aa3b96c996ba54456fe45
Jacob Cons <jacobcons@gmail.com>
2022-06-06 13:01:48 +0100

    Update 2022-06-06T12:01:34.866Z

--- _js/routes/common.js
+++
@@ -1,9 +0,0 @@
-import HamburgerNav from '../components/HamburgerNav'
-import SubNav from '../components/SubNav'
-
-export default {
-  init() {
-		const hamburgerNav = new HamburgerNav()
-    const subNav = new SubNav()
-  }
-};
--- _js/routes/videos.js
+++
@@ -1,8 +0,0 @@
-import Videos from '../components/Videos'
-
-export default {
-  async init() {
-    const videos = new Videos()
-    await videos.init()
-  }
-};
